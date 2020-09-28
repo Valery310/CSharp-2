1d01bce894ca808e691b133c14f03c3e39995a4c
valery310 <valery310@live.com>
2020-09-28 22:00:36 +0300

    Добавьте файлы проекта.

---
+++ Asteroid/Game.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Asteroid
+{
+    class Game
+    {
+        private static BufferedGraphicsContext _context;
+        public static BufferedGraphics Buffer;
+        public static BaseObject[] _objs;
+        public static BaseObject[] starField;
+        // Свойства
+        // Ширина и высота игрового поля
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+
+        static Game()
+        {
+        }
+
+        public static void Init(Form form)
+        {
+            // Графическое устройство для вывода графики            
+            Graphics g;
+            // Предоставляет доступ к главному буферу графического контекста для текущего приложения
+            _context = BufferedGraphicsManager.Current;
+            g = form.CreateGraphics();
+            // Создаем объект (поверхность рисования) и связываем его с формой
+            // Запоминаем размеры формы
+            Width = form.ClientSize.Width;
+            Height = form.ClientSize.Height;
+            // Связываем буфер в памяти с графическим объектом, чтобы рисовать в буфере
+            Buffer = _context.Allocate(g, new Rectangle(0, 0, Width, Height));
+            Load();
+            Timer timer = new Timer { Interval = 41 };
+            timer.Start();
+            timer.Tick += Timer_Tick;
+        }
+
+        private static void Timer_Tick(object sender, EventArgs e)
+        {
+            Draw();
+            Update();
+        }
+
+        public async static void Draw()
+        {
+            Buffer.Graphics.Clear(Color.Black);
+            foreach (BaseObject obj in _objs)
+                obj.Draw();
+            foreach (BaseObject star in starField)
+                star.Draw();
+            Buffer.Render();
+        }
+
+        public static void Update()
+        {
+            foreach (BaseObject obj in _objs)
+                obj.Update();
+            foreach (BaseObject star in starField)
+                star.Update();
+        }
+
+        public static void Load()
+        {
+            _objs = new BaseObject[30];
+            for (int i = 0; i < _objs.Length; i++)
+                _objs[i] = new Pic(new Point(600, i * 20), new Point(-i, -i), new Size(15, 15), @".\ufo.png");
+            //for (int i = _objs.Length / 2; i < _objs.Length; i++)
+            //    _objs[i] = new Star(new Point(600, i * 20), new Point(-i, 0), new Size(5, 5));
+
+            starField = new BaseObject[15000];
+            Random random = new Random();
+
+            for (int i = 0; i < starField.Length; i++)
+                starField[i] = new StarField(new Point(random.Next(-Width, Width), random.Next(-Height, Height)), new Point(10,10), new Size(10, 10), Width, Height,random.Next(1, Width));
+        }
+
+
+    }
+}
---
+++ Asteroid/Pic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asteroid
+{
+    class Pic : BaseObject
+    {
+        protected Bitmap bitmap;
+
+        public Pic(Point pos, Point dir, Size size, string filepath) : base (pos, dir, size)
+        {
+            bitmap = new Bitmap(filepath);
+        }
+
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.DrawImage(bitmap, Pos.X, Pos.Y, Size.Width, Size.Height);
+        }
+    }
+}
